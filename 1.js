f22d8f31e7d2032fce54b4bc0d9d0c6d48280d3d
rulamon <ruben.lamon@gmail.com>
2018-12-11 20:05:26 +0100

    condensed into single regex

--- 1.js
+++ 1.js
@@ -1,10 +1,8 @@
 function telephoneCheck(str) {
 	// define regex for all possibilities 
-	let regex = /^1?[-\s(]*\d{3}[-\s)]*\d{3}[-\s]*\d{4}$/;
-	// define seperate regex for non-closing parentheses
-	let regexBrackets = /\((?!.*\))|(?<!\(.*)\)/;
-	// return ternary operator, testing str against both regexes (regexBrackets must be inverted, will return true if not conform)
-	return !regexBrackets.test(str) && regex.test(str) ? true : false;
+	let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]*\d{3}[-\s]*\d{4}$/;
+	// return ternary operator, testing str against regex
+	return regex.test(str) ? true : false;
 }
 
-console.log(telephoneCheck("555-5555"));
+console.log(telephoneCheck("4567654323"));
